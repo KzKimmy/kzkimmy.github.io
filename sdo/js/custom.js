f0232ab3410f7336480e6679f2f3b9bc7b3b149e
KzKimmy <36092808+KzKimmy@users.noreply.github.com>
2021-03-25 01:35:08 +0700

    sdo dl

---
+++ sdo/js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    var classCycle = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6', 'img7', 'img8', 'img9', 'img10'];
+    var randomNumber = Math.floor(Math.random() * classCycle.length);
+    var classToAdd = classCycle[randomNumber];
+    $('body').addClass(classToAdd);
+});
